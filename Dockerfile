1923384d630fc51ececf968e7d094f854c7ac7b5
Axel Fontaine <axel@boxfuse.com>
2022-03-07 15:27:33 +0100

    Initial version

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM alpine:3.15.0 AS build
+
+ARG CONFIG_FILE
+RUN apk add linux-virt wget xz alpine-sdk flex bison bc linux-headers diffutils elfutils-dev gmp-dev mpc1-dev mpfr-dev openssl1.1-compat-dev findutils perl
+ADD $CONFIG_FILE /build/.config
+ADD kernel.license /build/LICENSE
+ADD build.sh /build
+RUN sh -c /build/build.sh
+
+
+FROM scratch AS export
+COPY --from=build /kernel.tar.gz .
---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+CONFIG=/build/.config
+VERSION=$(grep '# Linux/' $CONFIG | cut -d ' ' -f 3)
+VERSION_MAJOR=$(echo "$VERSION" | cut -b 1 )
+
+echo Downloading kernel "$VERSION" ...
+cd /build || exit
+wget https://cdn.kernel.org/pub/linux/kernel/v"$VERSION_MAJOR".x/linux-"$VERSION".tar.xz
+echo Extracting kernel "$VERSION" ...
+tar -xJf linux-"$VERSION".tar.xz
+mv linux-"$VERSION" linux
+cp .config linux/.config
+
+echo Building kernel ...
+cd /build/linux || exit
+
+make -j$(nproc) || exit
+mkdir /export
+find /build/linux -name bzImage -exec cp "{}" /export  \;
+
+if grep -q 'CONFIG_MODULES=y' $CONFIG; then
+  INSTALL_MOD_PATH=/export make modules_install
+  rm -Rf /export/lib/modules/*/build
+  rm -Rf /export/lib/modules/*/source
+fi
+
+echo Packaging kernel ...
+cd /export || exit
+cp /build/LICENSE .
+tar -czvf /kernel.tar.gz *
