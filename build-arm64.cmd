283fcc55cf2d24c855df76cee0fcbd8bbdcaca40
Axel Fontaine <axel@boxfuse.com>
2022-08-24 17:54:34 +0200

    Fixed ARCH conflict

--- build-arm64.cmd
+++ build-arm64.cmd
@@ -1,1 +1,1 @@
-docker buildx build --build-arg ARCH=arm64 --build-arg TAG=0 --platform=linux/arm64 --progress=plain --output type=local,dest=build/arm64 .
+docker buildx build --build-arg ARCH0=arm64 --build-arg TAG=0 --platform=linux/arm64 --progress=plain --output type=local,dest=build/arm64 .
--- build-x64.cmd
+++ build-x64.cmd
@@ -1,2 +1,1 @@
-docker buildx build --build-arg ARCH=x64 --build-arg TAG=0 --platform=linux/amd64 --progress=plain --output type=local,dest=build/x64 .
-copy /Y build\x64\kernel.tar.gz ..\nanoos
+docker buildx build --build-arg ARCH0=x64 --build-arg TAG=0 --platform=linux/amd64 --progress=plain --output type=local,dest=build/x64 .
