c582c7daf06ed7f42daa1bb3d618037fa354527d
Axel Fontaine <axel@boxfuse.com>
2023-01-24 11:52:09 +0100

    Reverted zstd compression

--- build.sh
+++ build.sh
@@ -18,9 +18,8 @@ echo Building kernel ...
 cd /build/linux || exit
 make -j"$(nproc)" || exit
 
-if [ -f arch/arm64/boot/Image ]; then
-  zstd arch/arm64/boot/Image
-  cp arch/arm64/boot/Image.zst bzImage
+if [ -f arch/arm64/boot/Image.gz ]; then
+  cp arch/arm64/boot/Image.gz bzImage
 fi
 
 mkdir /export
